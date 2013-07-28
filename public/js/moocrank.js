0ab557f599cc917520294dac56f012158bdb4562
Israel Gutiérrez <gootyfer@gmail.com>
2013-07-28 22:00:41 +0200

    add uservoice

--- public/js/moocrank.js
+++ public/js/moocrank.js
@@ -13,3 +13,20 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-42081482-1', 'moocrank.com');
 ga('send', 'pageview');
+
+<!-- UserVoice JavaScript SDK (only needed once on a page) -->
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/END4McFdFlPOtx5dSW2HJQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+
+<!-- A tab to launch the Classic Widget -->
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+  mode: 'full',
+  primary_color: '#cc6d00',
+  link_color: '#007dbf',
+  default_mode: 'support',
+  forum_id: 216610,
+  tab_label: 'Feedback & Support',
+  tab_color: '#cc6d00',
+  tab_position: 'middle-right',
+  tab_inverted: false
+}]);
